8637e8d5c92baf87db646ed8fa05c6dfe45837af
Adrien K <adrienkomaroff@hotmail.fr>
2023-02-01 16:25:46 +0100

    added param

--- robot_dynamic_param/src/server.cpp
+++ robot_dynamic_param/src/server.cpp
@@ -2,15 +2,15 @@
 
 #include <dynamic_reconfigure/server.h>
 #include <robot_dynamic_param/paramConfig.h>
-#include <std_msgs/Bool.h>
+#include <std_msgs/Int8.h>
 
 ros::NodeHandle* nh_glob;
 ros::Publisher* pub_glob;
-std_msgs::Bool msg;
+std_msgs::Int8 msg;
 
 void callback(robot_dynamic_param::paramConfig &config, uint32_t level) {
    
-  msg.data = !msg.data;
+  msg.data = (uint8_t) level;
   pub_glob->publish(msg);
 
 }
@@ -19,8 +19,7 @@ int main(int argc, char **argv) {
   ros::init(argc, argv, "robot_dynamic_param");
   ros::NodeHandle nh;  
   nh_glob = &nh;
-  ros::Publisher pub = nh.advertise<std_msgs::Bool>("parameter_update",20);
-  msg.data = false;
+  ros::Publisher pub = nh.advertise<std_msgs::Int8>("parameter_update",20);
   pub_glob = &pub;
   dynamic_reconfigure::Server<robot_dynamic_param::paramConfig> server;
   dynamic_reconfigure::Server<robot_dynamic_param::paramConfig>::CallbackType f;
